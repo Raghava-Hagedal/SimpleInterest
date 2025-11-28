e23fd23213d0b705e8a7787eb65123b340c1ca42
Raghava-Hagedal <raghavahagedal@gmail.com>
2025-11-28 22:14:19 +0530

    Update SimpleInterest.py

--- SimpleInterest.py
+++ SimpleInterest.py
@@ -1,16 +1,15 @@
 import sys
-if :
-len(sys_avg)==4:
-script_name=sys.avg[0]
-principal=sys.avg[1]
-rate=sys.avg[2]
-time=sys.avg[3]
+if len(sys.argv) == 4:
+    script_name = sys.argv[0]   # script name
+    principal = float(sys.argv[1])
+    rate = float(sys.argv[2])
+    time = float(sys.argv[3])
 else:
-principal=10000
-rate=5.0
-time=10
-simple_interest=(principal*rate*time)/100
-print("Principal Amount is:",principal)
-print("Rate of Interest is:",rate)
-print("Time Period is:",time)
-print("Simple Interest is:",simple_interest)
+    principal = 10000
+    rate = 5.0
+    time = 10
+simple_interest = (principal * rate * time) / 100
+print("Principal Amount is:", principal)
+print("Rate of Interest is:", rate)
+print("Time Period is:", time)
+print("Simple Interest is:", simple_interest)
